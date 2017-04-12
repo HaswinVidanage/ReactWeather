d1385d23072d1405e974e99d7df5ed7226048b5a
Haswin Vidanage <Haswin Vidanage>
2017-04-12 12:43:52 +0530

    created a modal component and fixed error

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -2,8 +2,10 @@ var React = require('react');
 var WeatherForm = require('WeatherForm');
 var WeatherMessage = require('WeatherMessage');
 var openWeatherMap = require('openWeatherMap');
+var ErrorModal = require('ErrorModal');
 
 var Weather = React.createClass({
+
   getInitialState:function(){
     return{
       isLoading: false
@@ -12,7 +14,10 @@ var Weather = React.createClass({
   handleSearch:function(location){
     var that = this;
 
-    this.setState({isLoading: true});
+    this.setState({
+      isLoading: true,
+      errorMessage: undefined
+    });
 
     openWeatherMap.getTemp(location).then(function(temp){
       that.setState({
@@ -20,12 +25,12 @@ var Weather = React.createClass({
         temp:temp,
         isLoading:false
       });
-    },function(errorMessage){
+    },function(e){
+      console.log(e);
       that.setState({
-
-        isLoading:false
+        isLoading:false,
+        errorMessage: e.message
       });
-      alert(errorMessage);
     });
 
 
@@ -33,8 +38,7 @@ var Weather = React.createClass({
   },
   render:function(){
 
-    var {isLoading, location, temp} = this.state;
-
+    var {isLoading, location, temp, errorMessage, title} = this.state;
     function renderMessage(){
         if(isLoading){
           return <h3 className="text-center">Fetching weather...</h3>;
@@ -43,12 +47,22 @@ var Weather = React.createClass({
         }
     }
 
+    function renderError(){
+      if(typeof errorMessage === 'string' ){
+        return(
+          <ErrorModal title={title} message={errorMessage}/>
+        )
+      }
+    }
+
     return(
       <div>
         <h1 className="text-center">Get Weather</h1>
 
         <WeatherForm onSearch={this.handleSearch}/>
         {renderMessage()}
+        {renderError()}
+
     </div>
     );
   }
